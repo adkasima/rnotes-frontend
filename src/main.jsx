7d939240f0748e1b089c92513c7e0c587dcc4e93
Adriel Kasima <adrielkasima@gmail.com>
2023-02-22 15:56:15 -0300

    add login function

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import ReactDOM from "react-dom/client"
 
 import theme from "./styles/theme"
 
+import { AuthProvider } from "./hooks/auth"
+
 import { Routes } from "./routes"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react"
 import { FiMail, FiLock } from "react-icons/fi"
 import { Link } from "react-router-dom"
+
+import { useAuth } from "../../hooks/auth"
 
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 import { Container, Form, Background } from "./styles"
 
 export function Signin() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({ email, password })
+
+  }
+
   return (
     <Container>
       <Form>
@@ -14,10 +27,20 @@ export function Signin() {
 
         <h2>Faça seu login</h2>
 
-        <Input placeholder="e-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="e-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar conta</Link>
       </Form>
